a77cba70518bf023691345a1c325d0815b7cd4e2
chrisdaff93 <99039245+chrisdaff93@users.noreply.github.com>
2024-02-26 15:29:42 +0100

    Changed testbench to send aditi instead of make

--- napoleon_tb.vhd
+++ napoleon_tb.vhd
@@ -15,10 +15,10 @@ architecture arch of napoleon_tb is
 constant clk_period : time := 10 ns;
 constant bit_period : time := 52083ns; -- time for 1 bit.. 1bit/19200bps = 52.08 us
 
-constant rx_data_ascii_m: std_logic_vector(7 downto 0) := x"6D"; -- receive m
-constant rx_data_ascii_a: std_logic_vector(7 downto 0) := x"61"; -- receive a
-constant rx_data_ascii_k: std_logic_vector(7 downto 0) := x"6B"; -- receive k
-constant rx_data_ascii_e: std_logic_vector(7 downto 0) := x"65"; -- receive e
+constant rx_data_ascii_a: std_logic_vector(7 downto 0) := x"61"; -- receive m
+constant rx_data_ascii_d: std_logic_vector(7 downto 0) := x"100"; -- receive a
+constant rx_data_ascii_i: std_logic_vector(7 downto 0) := x"69"; -- receive k
+constant rx_data_ascii_t: std_logic_vector(7 downto 0) := x"74"; -- receive e
 constant rx_data_ascii_enter: std_logic_vector(7 downto 0) := x"0D"; -- receive enter
 
 Component napoleon
@@ -51,46 +51,56 @@ begin
         reset <= '0';
         wait for clk_period*2;
         
-        -- Test ASCII char m
+        -- Test ASCII char a
                 srx <= '0'; -- start bit = 0
                 wait for bit_period;
                 for i in 0 to 7 loop
-                    srx <= rx_data_ascii_m(i);   -- 8 data bits
+                    srx <= rx_data_ascii_a(i);   -- 8 data bits
                     wait for bit_period;
                 end loop;
                 srx <= '1'; -- stop bit = 1
                 wait for 1ms;
         
-        -- Test ASCII char a
+        -- Test ASCII char d
                         srx <= '0';                      -- start bit = 0
                         wait for bit_period;
                         for i in 0 to 7 loop
-                            srx <= rx_data_ascii_a(i);   -- 8 data bits
+                            srx <= rx_data_ascii_d(i);   -- 8 data bits
                             wait for bit_period;
                         end loop;
                         srx <= '1';                      -- stop bit = 1
                         wait for 1ms;
 
-        -- Test ASCII char k
+        -- Test ASCII char i
                         srx <= '0';                      -- start bit = 0
                         wait for bit_period;
                         for i in 0 to 7 loop
-                            srx <= rx_data_ascii_k(i);   -- 8 data bits
+                            srx <= rx_data_ascii_i(i);   -- 8 data bits
                             wait for bit_period;
                         end loop;
                         srx <= '1';                      -- stop bit = 1
                         wait for 1ms;
  
-         -- Test ASCII char e
+         -- Test ASCII char t
                         srx <= '0';                      -- start bit = 0
                         wait for bit_period;
                         for i in 0 to 7 loop
-                            srx <= rx_data_ascii_e(i);   -- 8 data bits
+                            srx <= rx_data_ascii_t(i);   -- 8 data bits
                             wait for bit_period;
                         end loop;
                         srx <= '1';                      -- stop bit = 1
                         wait for 1ms;
-                                                
+
+        -- Test ASCII char i
+                        srx <= '0';                      -- start bit = 0
+                        wait for bit_period;
+                        for i in 0 to 7 loop
+                            srx <= rx_data_ascii_i(i);   -- 8 data bits
+                            wait for bit_period;
+                        end loop;
+                        srx <= '1';                      -- stop bit = 1
+                        wait for 1ms;
+                                                                        
          -- Test ACII Enter
                     srx <= '0';                      -- start bit = 0
                     wait for bit_period;
@@ -102,5 +112,4 @@ begin
                     wait;
        
         end process;
-
 end arch;
